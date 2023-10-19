684ab0896bc6e23618d72d8ea4092715021437b8
GaurAkshat1008 <akshatgauriiitsurat@gmail.com>
2023-10-19 10:54:52 +0530

    updated server.js and added README

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+let relayState = "OFF";
+
+app.use(express.static("public"));
+
+app.get("/update", (req, res) => {
+  const { temp, hum, moist, relay } = req.query;
+  console.log(`Temp: ${temp} Hum: ${hum} Moist: ${moist} Relay: ${relay}`);
+  relayState = relay === "HIGH" ? "ON" : "OFF";
+  res.sendStatus(200);
+});
+
+app.get("/override", (req, res) => {
+  relayState = relayState === "ON" ? "OFF" : "ON";
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
